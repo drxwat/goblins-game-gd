dcfd0b94e4b4df221e84b6d32c19b9aa923700ef
drxwat <drxwat@gmail.com>
2020-02-08 12:07:51 +0300

    enemy hunt radius increase

--- scripts/units/enemy.gd
+++ scripts/units/enemy.gd
@@ -1,12 +1,27 @@
 extends "./unit.gd"
 
 export (NodePath) var _aim_node_path
+export (float) var guard_radius = 350
+export (float) var guard_radius_hunt_multiplier = 1.5
 
 var _aim: Node2D
 var _follow_aim: bool = false
 
+var _idle_shape: CircleShape2D
+var _hunt_shape: CircleShape2D
+var _collision_shape: CollisionShape2D
+
 
 func _on_ready():
+	_idle_shape = CircleShape2D.new()
+	_idle_shape.radius = guard_radius
+	
+	_hunt_shape = CircleShape2D.new()
+	_hunt_shape.radius = guard_radius * guard_radius_hunt_multiplier
+	
+	_collision_shape = get_node("Area2D/CollisionShape2D")
+	_collision_shape.shape = _idle_shape
+	
 	if _aim_node_path:
 		_aim = get_node(_aim_node_path)
 	._on_ready()
@@ -18,13 +33,19 @@ func _on_process(delta):
 	._on_process(delta)
 
 
-func _on_Area2D_body_entered(_body):
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	if _follow_aim:
+		return
+	
+	_collision_shape.shape = _hunt_shape
 	_follow_aim = true
 	set_process(true)
-	pass # Replace with function body.
 
 
-func _on_Area2D_body_exited(_body):
+func _on_Area2D_body_shape_exited(body_id, body, body_shape, area_shape):
+	if not _follow_aim:
+		return
+	
+	_collision_shape.shape = _idle_shape
 	_follow_aim = false
 	set_process(false)
-	pass # Replace with function body.
