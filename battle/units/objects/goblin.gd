40e907bed2bd536fe499d749b146de86e9ec6be4
drxwat <drxwat@gmail.com>
2020-03-30 13:19:09 +0300

    external animations storage

--- battle/units/objects/goblin.gd
+++ battle/units/objects/goblin.gd
@@ -21,24 +21,6 @@ func move_along_path(delta) -> void:
 	else:
 		move_and_slide(move_vector.normalized() * SPEED * delta, Vector3(0, 1, 0))
 
-		
-
-
-#func move_along_distance(distance: float) -> void:
-#	var start_point := global_transform.origin
-#	for i in range(_path.size()):
-#		var distance_to_next := start_point.distance_to(_path[0])
-#		if distance <= distance_to_next and distance >= 0.0:
-#			var move_vec = start_point.linear_interpolate(_path[0], distance / distance_to_next)
-#			move_and_slide(move_vec.normalized(), Vector3(0, 1, 0))
-#			break
-#		elif distance < 0.0:
-#			move_and_slide(_path[0].normalized(), Vector3(0, 1, 0))
-#			set_process(false)
-#			break
-#		distance -= distance_to_next
-#		start_point = _path[0]
-#		_path.remove(0)
 
 func set_path(path: PoolVector3Array) -> void:
 	_path = path
