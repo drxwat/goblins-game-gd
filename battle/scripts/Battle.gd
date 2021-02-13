68c8f57f9446007045750102bfb40fbd312897ff
drxwat <drxwat@gmail.com>
2021-02-13 13:06:27 +0300

    battle units spawn

--- battle/scripts/Battle.gd
+++ battle/scripts/Battle.gd
@@ -4,26 +4,53 @@ const RAY_LENGTH = 1000
 const MOUSE_HOVER_Y_OFFSET = Vector3(0, 0.05, 0)
 
 onready var camera := $Camera
-onready var goblin := $BattleManager/BattleGoblin
+onready var goblin := $BattleGoblin
 onready var terrain := $Terrain
 onready var mouse_hover := $MouseHover
-onready var battle_manager := $BattleManager
 
-var team1_units = [
-	{ "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
-	{ "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
-	{ "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
-]
+var team1_units_meta = {
+	1: { "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
+	2: { "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
+	3: { "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
+}
+var team1_spawn_point = Vector3(1, 0, 9)
 
-var team2_units = [
-	{ "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
-	{ "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
-]
+var team2_units_meta = {
+	4: { "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
+	5: { "RACE": GlobalConstants.RACE.GOBLIN, "WEAPON": GlobalConstants.WEAPON.AXE },
+}
+var team2_spawn_point = Vector3(1, 0, 1)
+
+var team1 := {}
+var team2 := {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Terrain.set_obstacles($Forest)
-	battle_manager.initialize_battle(team1_units, team2_units, Vector3(1, 0, 9), Vector3(1, 0 ,1))
+	terrain.set_obstacles($Forest)
+	if !terrain.is_point_walkable(team1_spawn_point):
+		push_error("Team1 spawn point %s is not walkable" % team1_spawn_point)
+	if !terrain.is_point_walkable(team2_spawn_point):
+		push_error("Team2 spawn point %s is not walkable" % team2_spawn_point)
+	team1 = _init_team(team1_units_meta, team1_spawn_point, true)
+	team2 = _init_team(team2_units_meta, team2_spawn_point)
+#	battle_manager.initialize_battle(team1_units, team2_units, Vector3(1, 0, 9), Vector3(1, 0 ,1))
+
+# Creates and spawns units of the team
+func _init_team(units_meta: Dictionary, initial_spawn_point: Vector3, rotate = false) -> Dictionary:
+	var team := {}
+	var spawn_point = initial_spawn_point
+	for unit_id in units_meta.keys():
+		if spawn_point == null:
+			push_error("Team can't be spawned. Stoped at unit %s" % unit_id)
+			break
+		var unit_meta = units_meta.get(unit_id)
+		var team_unit_meta = unit_meta.duplicate()
+		var unit = _produce_unit(team_unit_meta)
+		_spawn_unit(unit, $Units, spawn_point, PI if rotate else 0)
+		team_unit_meta["UNIT"] = unit
+		team[unit_id] = team_unit_meta
+		spawn_point = terrain.get_neighbor_walkable_point(spawn_point)
+	return team
 
 func _input(event: InputEvent):
 	_handle_mouse_click(event)
@@ -62,4 +89,14 @@ func _get_mouse_projected_position(screen_position: Vector2):
 	if not result:
 		return null
 	return result.position
+
+func _produce_unit(unit_meta) -> BattleUnit:
+	var unit_scene = BattleConstants.RACES_SCENES[unit_meta["RACE"]]
+	var unit = unit_scene.instance()
+	unit.right_hand = unit_meta["WEAPON"]
+	return unit
 	
+func _spawn_unit(unit: BattleUnit, parent_node: Node, pos: Vector3, rot: float):
+	unit.translation = pos
+	unit.rotate_y(rot)
+	parent_node.add_child(unit)
--- battle/scripts/TerrainGridMap.gd
+++ battle/scripts/TerrainGridMap.gd
@@ -14,12 +14,36 @@ func get_map_path(from: Vector3, to: Vector3) -> PoolVector3Array:
 func get_map_cell_center(point: Vector3) -> Vector3:
 	return astar.get_point_position(astar.get_closest_point(point));
 	 
+func get_cell_position(cell: Vector3) -> Vector3:
+	var point = map_to_world(cell.x, cell.y, cell.z)
+	return astar.get_point_position(astar.get_closest_point(point)); 
 
 func set_obstacles(obstacles_map: GridMap):
 	for cell in obstacles_map.get_used_cells():
 		var point_id = astar.get_closest_point(obstacles_map.map_to_world(cell.x, cell.y, cell.z))
 		astar.set_point_disabled(point_id)
 
+func is_point_walkable(point: Vector3) -> bool:
+	return !astar.is_point_disabled(astar.get_closest_point(point))
+	
+func get_neighbor_walkable_point(point: Vector3):
+	var cell = world_to_map(point)
+	var neighbors = [
+		Vector3(1, 0, 0),
+		Vector3(0, 0, 1),
+		Vector3(1, 0, 1),
+		Vector3(-1, 0, 0),
+		Vector3(-1, 0, 1)
+	]
+	for neighbor in neighbors:
+		var n_cell = cell + neighbor
+		var n_point = map_to_world(n_cell.x, n_cell.y, n_cell.z)
+		var is_valid = get_cell_item(n_cell.x, n_cell.y, n_cell.z) != INVALID_CELL_ITEM
+		var is_walkable = is_point_walkable(n_point)
+		if is_valid and is_walkable:
+			return n_point
+	push_error("Point %s has no available neighbors" % point)
+	return null
 
 func _init_terrain_grid(astar):
 	var ground_cells = get_used_cells()
