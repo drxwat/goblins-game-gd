f99cd7a24d2637bb09d9d42b44de08f7460bbc9b
drxwat <drxwat@gmail.com>
2021-07-13 15:26:38 +0200

    animation end emitters & take damage animation

--- battle/units/battle-unit/BattleUnit.gd
+++ battle/units/battle-unit/BattleUnit.gd
@@ -1,17 +1,30 @@
 extends KinematicBody
 class_name BattleUnit
 
+#
+# This is basic battle unit class that assumes that there is GFX child node
+# in the child class that has AnimationTree, Tween and AnimationPlayer nodes
+# in the root. Child class cutomizes graphix but that graphix must follow the
+# restrictions. AnimationPlayer must contain basic anumations (see GoblinBattleUnit)
+# More over it must have bones for right and left hands. 
+#
+
 signal on_move_end
 signal on_attack_end
+signal on_take_damage_end
 signal on_dead
 
 const SPEED  := 300
 const MAX_HP := 20
 const LOCOMOTION_ANIMATION = "parameters/Locomotion/blend_amount"
-const MELE_ATTACK_ANIMATION = "parameters/Slash/active"
+const ACTIONS_ANIMATION = "parameters/Actions/playback"
 const MELE_ATTACK_ANIMATION_NAME = "slash"
+const TAKE_DAMAGE_ANIMATION_NAME = "react"
 const ANIMATION_TRANSITION = 0.4
 const ROTATION_TRANSITION = 0.1
+
+onready var animation_tree := $Gfx/AnimationTree
+onready var tween = $Gfx/Tween
 
 var _path: PoolVector3Array
 var _idle = true
@@ -19,9 +32,8 @@ var _is_selected := false setget set_selected
 var battle_id: int # available only after battle spawn
 var hp := MAX_HP
 var is_dead := false
-
-onready var animation_tree := $Gfx/AnimationTree
-onready var tween = $Gfx/Tween
+var current_animation
+var actions_state_machine
 
 export(GlobalConstants.WEAPON) var right_hand = GlobalConstants.WEAPON.NONE
 export (GlobalConstants.WEAPON) var left_hand = GlobalConstants.WEAPON.NONE
@@ -36,7 +48,8 @@ func _ready():
 		w_mesh.translation = w_meta.translation
 		w_mesh.rotation_degrees = w_meta.rotation_degrees
 		$Gfx/Armature/Skeleton/RightHandAttachment.add_child(w_mesh)
-
+	actions_state_machine = animation_tree[ACTIONS_ANIMATION]
+	_play_animation("idle-loop")
 
 func _physics_process(delta):
 	if is_dead:
@@ -59,7 +72,8 @@ func attack(unit: BattleUnit):
 	unit.take_damage(calculate_damage(self, unit))
 
 func mele_attack(unit: BattleUnit):
-	animation_tree[MELE_ATTACK_ANIMATION] = true
+	_play_action_animation(MELE_ATTACK_ANIMATION_NAME)
+	yield(get_tree().create_timer(0.6), "timeout") # TODO: Configure delays for all attack animations 
 	attack(unit)
 
 func range_attack(unit: BattleUnit):
@@ -69,6 +83,8 @@ func take_damage(damage: int):
 	hp -= damage
 	if hp <= 0:
 		die()
+		return
+	_play_action_animation(TAKE_DAMAGE_ANIMATION_NAME)
 
 func die():
 	is_dead = true
@@ -76,9 +92,31 @@ func die():
 func calculate_damage(attacker: BattleUnit, victim: BattleUnit):
 	return 3
 
-func handle_animation_finish(animation_name: String):
-	if animation_name == MELE_ATTACK_ANIMATION:
-		emit_signal("on_attack_end")
+func set_selected(value: bool):
+	_is_selected = value
+	if _is_selected:
+		$Selection.show()
+	else:
+		$Selection.hide()
+
+func _play_animation(animation_name):
+	actions_state_machine.start(animation_name)
+
+# state machine animation that requires polling to emit endsignal
+func _play_action_animation(animation_name):
+	current_animation = animation_name
+	$AnimationPoller.start()
+	_play_animation(animation_name)
+
+func _check_animation_end():
+	if actions_state_machine.get_current_node() != current_animation:
+		if current_animation == MELE_ATTACK_ANIMATION_NAME:
+			emit_signal("on_attack_end")
+		elif current_animation == TAKE_DAMAGE_ANIMATION_NAME:
+			emit_signal("on_take_damage_end")
+		current_animation = null
+	else:
+		$AnimationPoller.start()
 
 func _move_along_path(delta) -> void:
 	var move_vector: Vector3 = _path[0] - global_transform.origin
@@ -88,12 +126,6 @@ func _move_along_path(delta) -> void:
 		_rotate_unit(move_vector)
 		move_and_slide(move_vector.normalized() * SPEED * delta, Vector3(0, 1, 0))
 
-func set_selected(value: bool):
-	_is_selected = value
-	if _is_selected:
-		$Selection.show()
-	else:
-		$Selection.hide()
 
 func _play_acceleration_animation():
 	tween.interpolate_property(animation_tree, LOCOMOTION_ANIMATION, 
