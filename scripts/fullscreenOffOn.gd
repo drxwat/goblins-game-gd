a7a6ff9246c0f611bc29e612c127e3f6a48c6c81
drxwat <drxwat@gmail.com>
2021-08-03 09:54:39 +0200

    fullscreen moved to scripts & linux build imports

--- scripts/fullscreenOffOn.gd
+++ scripts/fullscreenOffOn.gd
@@ -5,7 +5,7 @@ extends Node
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
-func _physics_process(_delta):
+func _unhandled_input(event):
 	if Input.is_action_pressed("Alt") and Input.is_action_just_pressed("ui_accept"):
 		OS.window_fullscreen = not OS.window_fullscreen
 	elif Input.is_action_just_pressed("F11"):
