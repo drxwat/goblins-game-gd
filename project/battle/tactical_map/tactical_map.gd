2aeb1c4c2621bc7c95a3bc326f5a4771c54a8d43
Terminator <fresh-ter@yandex.com>
2021-08-07 14:48:52 +0400

    Update tactical_map.tscn

--- project/battle/tactical_map/tactical_map.gd
+++ project/battle/tactical_map/tactical_map.gd
@@ -1,189 +1,208 @@
 extends Spatial
 
 
-const item_soil = {
-	"dirt": 0, 
-	"grass": 1,
+enum ITEM_SOIL { DIRT, GRASS}
+enum ITEM_OBSTACLES {
+	DEAD_OAK_1,
+	DEAD_OAK_2,
+	DEAD_OAK_3,
+	
+	DEAD_SPRUCE_1,
+	DEAD_SPRUCE_2,
+	DEAD_SPRUCE_3,
+	
+	OAK_TREE_1,
+	OAK_TREE_2,
+	OAK_TREE_3,
+	
+	SPRUCE_TREE_1,
+	SPRUCE_TREE_2,
+	SPRUCE_TREE_3,
+	
+	STONE_1,
+	STONE_2,
+	STONE_3
 }
-const item_obstacles = {
-	"DeadOak1": 0,
-	"DeadOak2": 1,
-	"DeadOak3": 2,
+enum ITEM_VEGETATION {
+	GRASS_1,
+	GRASS_2,
+	GRASS_3,
+	GRASS_4,
+	GRASS_5,
+	GRASS_6,
 	
-	"DeadSpruce1": 3,
-	"DeadSpruce2": 4,
-	"DeadSpruce3": 5,
-	
-	"OakTree1": 6,
-	"OakTree2": 7,
-	"OakTree3": 8,
-	
-	"SpruceTree1": 9,
-	"SpruceTree2": 10,
-	"SpruceTree3": 11,
-	
-	"stone_2": 12,
-	"stone_3": 13,
-	"stones_1": 14,
-}
-const item_vegetation = {
-	"Grass_01": 0,
-	"Grass_02": 1,
-	"Grass_03": 2,
-	"Grass_04": 3,
-	"Grass_05": 4,
-	"Grass_06": 5,
-	
-	"Mushroom_01": 6,
-	"Mushroom_02": 7,
-	"Mushroom_03": 8,
-	"Mushroom_04": 9,
+	MUSHROOM_1,
+	MUSHROOM_2,
+	MUSHROOM_3,
+	MUSHROOM_4
 }
 
-onready var map_height: int = 100
-onready var map_widht: int = 100
+
+var map_height: int = 100
+var map_widht: int = 100
 
 
 func _ready():
-	_create_forest_glade()
+	var randomMapGenerator = RandomMapGenerator.new(map_height, map_widht,
+		$Soil, $Obstacles, $Vegetation)
+	randomMapGenerator._create_forest_glade()
 
 
-func _create_forest_glade() -> void:
-	# SOIL ITEMS
-	var grass_cells = _generate_item_coordinates(2)
-	_set_soil_cells_item(grass_cells, item_soil["grass"])
+class RandomMapGenerator:
+	var map_height: int
+	var map_widht: int
 	
-	var dirt_cells = _generate_item_coordinates(0.3)
-	_set_soil_cells_item(dirt_cells, item_soil["dirt"])
+	var soil: GridMap
+	var obstacles: GridMap
+	var vegetation: GridMap
 	
-	# OBSTACLES ITEMS
-	var dead_oak_cells = _generate_item_coordinates(0.001)
-	_set_obstacles_cells_items(dead_oak_cells, item_obstacles["DeadOak1"],
-		item_obstacles["DeadOak2"], item_obstacles["DeadOak3"]
-	)
-	
-	var dead_spruce_cells = _generate_item_coordinates(0.001)
-	_set_obstacles_cells_items(dead_spruce_cells, item_obstacles["DeadSpruce1"],
-		item_obstacles["DeadSpruce2"], item_obstacles["DeadSpruce3"]
-	)
-	
-	var oak_tree_cells = _generate_item_coordinates(0.01)
-	_set_obstacles_cells_items(oak_tree_cells, item_obstacles["OakTree1"],
-		item_obstacles["OakTree2"], item_obstacles["OakTree3"]
-	)
-	
-	var spruce_tree_cells = _generate_item_coordinates(0.01)
-	_set_obstacles_cells_items(spruce_tree_cells, item_obstacles["SpruceTree1"],
-		item_obstacles["SpruceTree2"], item_obstacles["SpruceTree3"]
-	)
-	
-	var stone_cells = _generate_item_coordinates(0.003)
-	_set_obstacles_cells_items(stone_cells, item_obstacles["stone_2"],
-		item_obstacles["stone_3"], item_obstacles["stones_1"]
-	)
-	
-	# VEGETATION ITEMS
-#	var grass_v_cells = _generate_item_coordinates(0.001,
-#			map_widht*5, map_height*5)
-#	_set_vegetation_grass_cells_items(grass_v_cells, 
-#		item_vegetation["Grass_01"], item_vegetation["Grass_02"],
-#		item_vegetation["Grass_03"], item_vegetation["Grass_04"],
-#		item_vegetation["Grass_05"], item_vegetation["Grass_06"]
-#	)
-#
-#	var mushrooms_cells = _generate_item_coordinates(0.01,
-#			map_widht, map_height)
-#	_set_mushrooms_cells_items(mushrooms_cells, 
-#		item_vegetation["Mushroom_01"], item_vegetation["Mushroom_02"],
-#		item_vegetation["Mushroom_03"], item_vegetation["Mushroom_04"]
-#	)
+	func _init(_map_height, _map_widht, _soil, _obstacles, _vegetation):
+		map_height =_map_height
+		map_widht =_map_widht
+		
+		soil = _soil
+		obstacles = _obstacles
+		vegetation = _vegetation
+		
+	func _create_forest_glade() -> void:
+		# SOIL ITEMS
+		var grass_cells = _generate_item_coordinates(2)
+		_set_soil_cells_item(grass_cells, ITEM_SOIL.GRASS)
+		
+		var dirt_cells = _generate_item_coordinates(0.3)
+		_set_soil_cells_item(dirt_cells, ITEM_SOIL.DIRT)
+		
+		# OBSTACLES ITEMS
+		var dead_oak_cells = _generate_item_coordinates(0.001)
+		_set_obstacles_cells_items(dead_oak_cells, ITEM_OBSTACLES.DEAD_OAK_1,
+			ITEM_OBSTACLES.DEAD_OAK_2, ITEM_OBSTACLES.DEAD_OAK_3
+		)
+		
+		var dead_spruce_cells = _generate_item_coordinates(0.001)
+		_set_obstacles_cells_items(dead_spruce_cells, ITEM_OBSTACLES.DEAD_SPRUCE_1,
+			ITEM_OBSTACLES.DEAD_SPRUCE_2, ITEM_OBSTACLES.DEAD_SPRUCE_3
+		)
+		
+		var oak_tree_cells = _generate_item_coordinates(0.01)
+		_set_obstacles_cells_items(oak_tree_cells, ITEM_OBSTACLES.OAK_TREE_1,
+			ITEM_OBSTACLES.OAK_TREE_2, ITEM_OBSTACLES.OAK_TREE_3
+		)
+		
+		var spruce_tree_cells = _generate_item_coordinates(0.01)
+		_set_obstacles_cells_items(spruce_tree_cells, ITEM_OBSTACLES.SPRUCE_TREE_1,
+			ITEM_OBSTACLES.SPRUCE_TREE_2, ITEM_OBSTACLES.SPRUCE_TREE_3
+		)
+		
+		var stone_cells = _generate_item_coordinates(0.003)
+		_set_obstacles_cells_items(stone_cells, ITEM_OBSTACLES.STONE_1,
+			ITEM_OBSTACLES.STONE_2, ITEM_OBSTACLES.STONE_3
+		)
+		
+		# VEGETATION ITEMS
+	#	var grass_v_cells = _generate_item_coordinates(0.001,
+	#			map_widht*5, map_height*5)
+	#	_set_vegetation_grass_cells_items(grass_v_cells, 
+	#		item_vegetation["Grass_01"], item_vegetation["Grass_02"],
+	#		item_vegetation["Grass_03"], item_vegetation["Grass_04"],
+	#		item_vegetation["Grass_05"], item_vegetation["Grass_06"]
+	#	)
+	#
+	#	var mushrooms_cells = _generate_item_coordinates(0.01,
+	#			map_widht, map_height)
+	#	_set_mushrooms_cells_items(mushrooms_cells, 
+	#		item_vegetation["Mushroom_01"], item_vegetation["Mushroom_02"],
+	#		item_vegetation["Mushroom_03"], item_vegetation["Mushroom_04"]
+	#	)
 
 
-func _set_soil_cells_item(_cells: Array, _item: int) -> void:
-	for cell in _cells:
-		$Soil.set_cell_item(cell[0], 0, cell[1], _item)
+	func _set_soil_cells_item(_cells: Array, _item: int) -> void:
+		for cell in _cells:
+			soil.set_cell_item(cell[0], 0, cell[1], _item)
 
 
-func _set_obstacles_cells_items(
-		_cells: Array,
-		_item1: int,
-		_item2: int,
-		_item3: int
-	) -> void:
-	var random_index: int 
-	for cell in _cells:
-		random_index = randi() % 3
-		match random_index:
-			0:
-				$Obstacles.set_cell_item(cell[0], 0, cell[1], _item1)
-			1:
-				$Obstacles.set_cell_item(cell[0], 0, cell[1], _item2)
-			2:
-				$Obstacles.set_cell_item(cell[0], 0, cell[1], _item3)
+	func _set_obstacles_cells_items(
+			_cells: Array,
+			_item1: int,
+			_item2: int,
+			_item3: int
+		) -> void:
+		var random_index: int 
+		for cell in _cells:
+			random_index = randi() % 3
+			var r = Basis(Vector3(0,1,0), fmod(randf(), 2*PI))
+			print("QWERTY: ", r)
+			r = r.get_orthogonal_index()
+			print(r)
+			match random_index:
+				0:
+					obstacles.set_cell_item(cell[0], 0, cell[1], _item1, r)
+				1:
+					obstacles.set_cell_item(cell[0], 0, cell[1], _item2, r)
+				2:
+					obstacles.set_cell_item(cell[0], 0, cell[1], _item3, r)
 
 
-func _set_vegetation_grass_cells_items(
-		_cells: Array,
-		_item1: int,
-		_item2: int,
-		_item3: int,
-		_item4: int,
-		_item5: int,
-		_item6: int
-	) -> void:
-	var random_index: int 
-	for cell in _cells:
-		random_index = randi() % 6
-		match random_index:
-			0:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item1)
-			1:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item2)
-			2:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item3)
-			3:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item4)
-			4:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item5)
-			5:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item6)
+	func _set_vegetation_grass_cells_items(
+			_cells: Array,
+			_item1: int,
+			_item2: int,
+			_item3: int,
+			_item4: int,
+			_item5: int,
+			_item6: int
+		) -> void:
+		var random_index: int 
+		for cell in _cells:
+			random_index = randi() % 6
+			match random_index:
+				0:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item1)
+				1:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item2)
+				2:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item3)
+				3:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item4)
+				4:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item5)
+				5:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item6)
 
 
-func _set_mushrooms_cells_items(
-		_cells: Array,
-		_item1: int,
-		_item2: int,
-		_item3: int,
-		_item4: int
-	) -> void:
-	var random_index: int 
-	for cell in _cells:
-		random_index = randi() % 6
-		match random_index:
-			0:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item1)
-			1:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item2)
-			2:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item3)
-			3:
-				$Vegetation.set_cell_item(cell[0], 0, cell[1], _item4)
+	func _set_mushrooms_cells_items(
+			_cells: Array,
+			_item1: int,
+			_item2: int,
+			_item3: int,
+			_item4: int
+		) -> void:
+		var random_index: int 
+		for cell in _cells:
+			random_index = randi() % 6
+			match random_index:
+				0:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item1)
+				1:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item2)
+				2:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item3)
+				3:
+					vegetation.set_cell_item(cell[0], 0, cell[1], _item4)
 
 
-func _generate_item_coordinates(weight: float,
-		_map_widht=map_widht, _map_height=map_height) -> Array:
-	var result: Array
-	
-	for x in _map_widht:
-		for y in _map_height:
-			var r = randf()
-			if r < weight:
-				var c = _normalize_coordinates(x, y)
-				result.append(c)
-	return result
+	func _generate_item_coordinates(weight: float,
+			_map_widht=map_widht, _map_height=map_height) -> Array:
+		var result: Array
+		
+		for x in _map_widht:
+			for y in _map_height:
+				var r = randf()
+				if r < weight:
+					var c = offset_to_origin(x, y)
+					result.append(c)
+		return result
 
-
-func _normalize_coordinates(x: int, y: int) -> Array:
-	var x_norm: int = x - map_widht/2
-	var y_norm: int = y - map_height/2
-	return [x_norm, y_norm]
+	func offset_to_origin(x: int, y: int) -> Array:
+		var x_norm: int = x - map_widht/2
+		var y_norm: int = y - map_height/2
+		return [x_norm, y_norm]
