d0e53a20ad7a7cbbfc3a36f714004a9e6137a6ab
Dmitriy Derbin <dds9435@gmail.com>
2021-08-08 19:04:19 +0300

    separated postbattle timeout

--- project/global_map/units/player/player.gd
+++ project/global_map/units/player/player.gd
@@ -2,16 +2,14 @@ extends Unit
 
 signal contacted_enemy(player, enemy)
 
-var is_post_battle_timeout = false
-var post_battle_timeout = 2 
-
 func _on_ContactArea_area_shape_entered(area_id, area, area_shape, local_shape):
-	if not(is_post_battle_timeout):
-		if area.name == 'ContactArea':
-			if area.owner.name == 'Enemy':
-				print(area)
-				is_post_battle_timeout = true
-				emit_signal("contacted_enemy", self, area)
-				get_tree().paused = true
-				yield(get_tree().create_timer(post_battle_timeout, false), "timeout")
-				is_post_battle_timeout = false
+	if area:
+		if is_instance_valid(area):
+			var enemy = area.owner
+			if not(enemy.is_post_battle_timeout):
+				if area.name == 'ContactArea':
+					enemy.is_post_battle_timeout = true
+					emit_signal("contacted_enemy", self, enemy)
+					get_tree().paused = true
+					yield(get_tree().create_timer(enemy.post_battle_timeout, false), "timeout")
+					enemy.is_post_battle_timeout = false
