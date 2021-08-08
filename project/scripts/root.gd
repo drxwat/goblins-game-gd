9aef31f3899520fc44acb0fd7586bc71f61e3c0b
drxwat <drxwat@gmail.com>
2021-08-08 15:05:01 +0200

    moved global map files to subfolder

--- project/scripts/root.gd
+++ project/scripts/root.gd
@@ -5,15 +5,8 @@ onready var player: Node2D = get_node("Player")
 onready var tile_map: Node2D = get_node("TileMap")
 onready var units_parent = get_node("Units");
 
-onready var enemy_class := load(GlobalConstants.ENEMY_SCENE_PATH)
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var enemy: Node2D = enemy_class.instance()
-	units_parent.add_child(enemy)
-	enemy.position = Vector2(0, 0)
-	enemy.base_speed = enemy.base_speed / 1.5
-	enemy._aim = player
 	set_process(true)
 
 
--- project/scripts/tile_map.gd
+++ project/scripts/tile_map.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name GlobalTerrainTileMap
 
 enum WeightScales {
 	ROAD = 2,
