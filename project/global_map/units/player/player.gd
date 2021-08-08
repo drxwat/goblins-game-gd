e26e4eb3c58a687c0d313f0286634e3ad4f06758
Dmitriy Derbin <dds9435@gmail.com>
2021-08-08 18:39:08 +0300

    hotfix battle timeout

--- project/global_map/units/player/player.gd
+++ project/global_map/units/player/player.gd
@@ -1,11 +1,17 @@
 extends Unit
 
-signal contacted_enemy(enemy)
+signal contacted_enemy(player, enemy)
+
+var is_post_battle_timeout = false
+var post_battle_timeout = 2 
 
 func _on_ContactArea_area_shape_entered(area_id, area, area_shape, local_shape):
-	if area.name == 'ContactArea':
-		if area.owner.name == 'Enemy':
-			print(area)
-			emit_signal("contacted_enemy", area)
-			get_tree().paused = true
-			yield(get_tree().create_timer(2, false), "timeout")
+	if not(is_post_battle_timeout):
+		if area.name == 'ContactArea':
+			if area.owner.name == 'Enemy':
+				print(area)
+				is_post_battle_timeout = true
+				emit_signal("contacted_enemy", self, area)
+				get_tree().paused = true
+				yield(get_tree().create_timer(post_battle_timeout, false), "timeout")
+				is_post_battle_timeout = false
