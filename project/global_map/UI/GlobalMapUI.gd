223fd21ccb255e07fa2afb93dada5399e217a732
Dmitriy Derbin <dds9435@gmail.com>
2021-08-08 18:01:08 +0300

    prototype pre battle dialog

---
+++ project/global_map/UI/GlobalMapUI.gd
@@ -0,0 +1,11 @@
+extends Control
+
+onready var PreBattleDialog = $"PreBattleDialog"
+
+func _on_contacted_enemy(enemy):
+	PreBattleDialog.visible = true
+
+
+func _on_ButtonExit_pressed():
+	PreBattleDialog.visible = false
+	get_tree().paused = false
