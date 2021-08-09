456442b5dfbf3c5f9aea82e9d88534557dab6135
Dmitriy Derbin <dds9435@gmail.com>
2021-08-09 10:38:43 +0300

    menu logic

---
+++ project/main_menu/main_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+var visible = true setget set_visible
+
+func set_visible(val):
+	visible = val
+	$"MenuUI".visible = val
+
+
+func _on_Button_Exit_pressed():
+	get_tree().quit()
+
+
+func _on_Button_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button_Game_pressed():
+	get_tree().change_scene("res://global_map/global_map.tscn")
+
+
+func _ready():
+	get_tree().connect("screen_resized", self, "_screen_resized")
+	_screen_resized()
+
+
+func _screen_resized():
+	$"Viewport".size = get_viewport().size
