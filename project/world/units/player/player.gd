d72720265af5626de1922b804625e010d99bc09f
drxwat <drxwat@gmail.com>
2021-08-13 22:23:28 +0200

    demo 3d render to isometric

--- project/world/units/player/player.gd
+++ project/world/units/player/player.gd
@@ -2,6 +2,8 @@ extends Troop
 
 signal contacted_enemy(player, enemy)
 
+onready var gfx := $GFX
+
 func _on_ContactArea_area_shape_entered(area_id, area, area_shape, local_shape):
 	if area:
 		if is_instance_valid(area):
@@ -13,3 +15,14 @@ func _on_ContactArea_area_shape_entered(area_id, area, area_shape, local_shape):
 					get_tree().paused = true
 					yield(get_tree().create_timer(enemy.post_battle_timeout, false), "timeout")
 					enemy.is_post_battle_timeout = false
+
+
+func _on_process(delta: float):
+	._on_process(delta)
+	if is_moving():
+		_play_move_animation(position.direction_to(_path[0]))
+
+
+func _play_move_animation(direction: Vector2):
+	gfx.play("walk_down" if direction.y > 0 else "walk_up")
+	gfx.flip_h = true if direction.x > 0 else false
